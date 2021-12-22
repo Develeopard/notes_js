2063a8733982f7313135fb648f394e530eb14778
Develeopard <frontendwebdeveleopard@gmail.com>
2021-12-22 23:09:57 +0100

    added local storage

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const addBtn = document.querySelector('#add-btn');
 
+const notes = JSON.parse(localStorage.getItem('notes'))
 
+
+if(notes){
+    notes.forEach((note) => {
+        addNewNote(note)
+    })
+}
 
 addBtn.addEventListener('click', () => {
     addNewNote();
-})
+});
 
-function addNewNote(){
+function addNewNote(text = ''){
     const note = document.createElement('div');
     note.classList.add('note')
 
@@ -15,8 +22,8 @@ function addNewNote(){
         <button id="edit"><i class="fas fa-edit"></i></button>
         <button id="delete"><i class="fas fa-trash"></i></button>
     </div>
-    <div class="markdown hide" id="markdown"></div>
-    <textarea id="textarea"></textarea>
+    <div class="markdown ${text ? '' : 'hide'}" id="markdown"></div>
+    <textarea id="textarea" class="${text ? 'hide' : ''}"></textarea>
     </div>`;
 
     const editBtn = note.querySelector('#edit');
@@ -25,6 +32,9 @@ function addNewNote(){
     const textArea = note.querySelector('#textarea');
     const markdown = note.querySelector('#markdown');
 
+    textArea.value = text;
+    markdown.innerHTML = marked.parse(text);
+
     editBtn.addEventListener('click', () => {
         textArea.classList.toggle('hide');
         markdown.classList.toggle('hide')
@@ -32,12 +42,28 @@ function addNewNote(){
 
     deleteBtn.addEventListener('click', () => {
         note.remove();
+
+        updateLS();
     })
 
     textArea.addEventListener('input', (e) => {
         const { value } = e.target;
         markdown.innerHTML = marked.parse(value);
+
+        updateLS()
     })
 
     document.body.appendChild(note);
+}
+
+function updateLS(){
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    notesText.forEach((note) => {
+        notes.push(note.value)
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes));
 }
